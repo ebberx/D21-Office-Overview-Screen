94c885289153fd2c06269defccc0552ddf8b4843
Esben Christensen <71466904+blexaer@users.noreply.github.com>
2022-06-15 12:42:27 +0200

    fixed test

--- src/Test/ScheduleControllerTest.java
+++ src/Test/ScheduleControllerTest.java
@@ -18,19 +18,19 @@ public class ScheduleControllerTest {
     // timeToCanvasX
     @Test
     public void timeToCanvasXTest01() {
-        double value = ScheduleController.timeToCanvasX(LocalDateTime.now(), 1200);
+        double value = ScheduleController.timeToCanvasX(LocalDateTime.now(), canvasWidth);
         double expected = canvasWidth / 8 * 2;
         assertEquals(expected, value, 0.1);
     }
     @Test
     public void timeToCanvasXTest02() {
-        double value = ScheduleController.timeToCanvasX(LocalDateTime.now().plusHours(1), 1200);
+        double value = ScheduleController.timeToCanvasX(LocalDateTime.now().plusHours(1), canvasWidth);
         double expected = canvasWidth / 8 * 3;
         assertEquals(expected, value, 0.1);
     }
     @Test
     public void timeToCanvasXTest03() {
-        double value = ScheduleController.timeToCanvasX(LocalDateTime.now().minusHours(1), 1200);
+        double value = ScheduleController.timeToCanvasX(LocalDateTime.now().minusHours(1), canvasWidth);
         double expected = canvasWidth / 8;
         assertEquals(expected, value, 0.1);
     }
